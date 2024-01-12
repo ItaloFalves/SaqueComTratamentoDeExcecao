6cd1fc10ed9101b70f7051eaf7da0421dd68c9b6
italofalves <italofalves@hotmail.com>
2024-01-12 02:46:04 -0300

    Saque com tratamento de excecoes

---
+++ SaqueDeConta/SaqueDeConta/Entities/Conta.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaqueDeConta.Entities
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteDeSaque { get; set; }
+
+        public Conta()
+        {
+
+        }
+
+        public Conta(int numero, string titular, double saldo, double limiteDeSaque)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+            LimiteDeSaque = limiteDeSaque;
+        }
+
+        public void Depositar(double deposito)
+        {
+            Saldo += deposito;
+        }
+
+        public void Saque(double saque)
+        {
+            if (LimiteDeSaque < saque)
+            {
+                throw new DomainException("Limite para saque excedido! ");
+            }
+            else if (Saldo <= 0 || saque > Saldo)
+            {
+                throw new DomainException("Não há saldo suficiente para realizar o saque! ");
+            }
+            else
+            {
+                Saldo -= saque;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Novo saldo da conta: "
+                + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ SaqueDeConta/SaqueDeConta/Program.cs
@@ -0,0 +1,42 @@
+﻿using SaqueDeConta.Entities;
+using System;
+
+namespace SaqueDeConta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entre com os dados da conta: ");
+                Console.Write("Numero da conta: ");
+                int numConta = int.Parse(Console.ReadLine());
+                Console.Write("Entre com o titular da conta: ");
+                string titular = Console.ReadLine();
+                Console.Write("Entre com o saldo inicial: ");
+                double saldo = double.Parse(Console.ReadLine());
+                Console.Write("Entre com o limite de saque: ");
+                double limiteSaque = double.Parse(Console.ReadLine());
+
+                Conta conta = new Conta(numConta, titular, saldo, limiteSaque);
+
+                Console.WriteLine();
+                Console.Write("Entre com a quantidade para saque: ");
+                double saque = double.Parse(Console.ReadLine());
+                conta.Saque(saque);
+
+                Console.WriteLine();
+                Console.WriteLine(conta.ToString());
+            } 
+            catch (DomainException e)
+            {
+                Console.WriteLine("Erro na transação: " + e.Message);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("Erro inesperado: " + e.Message);
+            }
+        }
+    }
+}
